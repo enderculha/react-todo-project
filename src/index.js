f2e5b21e47363cff3bc731da28d91de6a5b0eead
Ender Culha <enderculha@gmail.com>
2022-04-11 17:03:59 +0200

    handle github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import {HashRouter as Router, Route, Routes} from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import About from "./functionBased/pages/About"
 import NotMatch from "./functionBased/pages/NotMatch"
 
@@ -14,7 +14,7 @@ import Navbar from "./functionBased/components/Navbar";
 
 ReactDOM.render(
    <React.StrictMode>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
          <Navbar/>
          <Routes>
             <Route path="/" element={<TodoContainer/>}></Route>
