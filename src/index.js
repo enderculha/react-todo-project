0a45f3c406cbc9a24ba2254fe387c666f8fa36f1
Ender Culha <enderculha@gmail.com>
2022-04-11 16:55:25 +0200

    router type changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import {HashRouter as Router, Route, Routes} from "react-router-dom"
 import About from "./functionBased/pages/About"
 import NotMatch from "./functionBased/pages/NotMatch"
 
